f5d536472fd3cbbff3ba115ab59fa727736c092e
Fathima <fathimasadakkathullah@gmail.com>
2026-04-20 10:37:01 +0530

    Time: 25 ms (10.01%) | Memory: 57.4 MB (10.52%) - LeetSync

---
+++ 69-sqrtx/sqrtx.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function (x) {
+    if (x <= 1) {
+        return x
+    }
+    for (let i = 1; i <= x; i++) {
+        if (i * i > x) {
+            return i - 1
+        }
+    }
+};
